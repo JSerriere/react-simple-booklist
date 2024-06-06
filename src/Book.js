7f9cea14685b30e0f135eedb6e99a5b04f44957a
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-06 16:17:52 +0200

    Add number to Book

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,10 @@
-export const Book = ({ img, title, author }) => {
+export const Book = ({ img, title, author, number }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
+      <span className="number">{`# ${number + 1}`}</span>
     </article>
   );
 };
