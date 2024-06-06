cb599b196c2410e7d6905c99288486170550e3e1
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-06 15:39:42 +0200

    Move const books from index.js to books.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
 
-const books = [
-  {
-    author: "Jordan Moore",
-    title: "Interesting Facts For Curious Minds",
-    img: "./images/book-1.jpg",
-    id: 1,
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-    id: 2,
-  },
-];
-
 const BookList = () => {
   return (
     <section className="booklist">
