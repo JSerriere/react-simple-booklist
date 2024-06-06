3a57e5406f5cca3871e54ab5c966927aa987e996
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-06 16:26:37 +0200

    Fix Book.js import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
 import {books} from './books';
-import {Book} from './Book'
+import Book from './Book'
 
 const BookList = () => {
   return (
