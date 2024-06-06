fae9f3ebc024ea246b5960d35a672eb1f047bedf
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-06 12:49:31 +0200

    Creating BookList

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
+  },
+];
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
