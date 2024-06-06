f88ed45d123eab2d57ba03177fa011ed6d8c18d8
Jerome Serriere <155807922+JSerriere@users.noreply.github.com>
2024-06-06 15:46:30 +0200

    Add import Book.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
 import {books} from './books';
+import {Book} from './Book'
 
 const BookList = () => {
   return (
@@ -12,7 +13,6 @@ const BookList = () => {
     </section>
   );
 };
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
